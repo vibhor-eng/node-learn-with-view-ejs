7a4776d53e5220493dd5424988657aa5e8e5f3c5
vibhor-eng <mayurty88@gmail.com>
2025-02-03 10:23:05 +0530

    some fixes and home url redirect to login

--- src/app.js
+++ src/app.js
@@ -25,6 +25,7 @@ app.set('view engine', 'ejs');
 // load the folder for view so that we can load view with name directly
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
+//to load view folder
 app.set('views', [path.join(__dirname, 'views'),path.join(__dirname, 'views/auth'),path.join(__dirname, 'views/layouts'),path.join(__dirname, 'views/layouts/blocks')]);
 
 // get images/css/js from public folder
@@ -39,7 +40,12 @@ app.use((req, res, next) => {
 
 import userRouter from './routes/user.router.js'
 
-//routes declaration
+//redirect to login when write home url
+app.get('/', (req, res) => {
+  res.redirect('/user/login');  // Redirect to /new-route
+});
+
+//routes declaration user is prefix here
 app.use("/user",userRouter)
 
  
